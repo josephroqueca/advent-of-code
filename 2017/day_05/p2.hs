dcdac3fd8868f33cf34c1c2f16aa234eee8505a9
Joseph Roque <contact@josephroque.ca>
2018-11-24 14:44:21 -0500

    2017, day 5, haskell

---
+++ 2017/day_05/p2.hs
@@ -0,0 +1,22 @@
+import Data.Map (Map, fromList, insert, size, (!))
+import qualified Data.Map as Map
+import System.IO
+
+stepThrough :: Map Int Int -> Int -> Int
+stepThrough offsets position = stepThrough' offsets position 0
+
+stepThrough' :: Map Int Int -> Int -> Int -> Int
+stepThrough' offsets position stepsSoFar
+    | position <= lowerBound || position >= upperBound = stepsSoFar
+    | otherwise = let newOffsets = insert position adjustedOffset offsets in stepThrough' newOffsets (position + currentOffset) (stepsSoFar + 1)
+    where currentOffset = offsets ! position
+          adjustedOffset = if (currentOffset >= 3) then currentOffset - 1 else currentOffset + 1
+          lowerBound = -1
+          upperBound = size offsets
+
+main :: IO()
+main = do
+    input <- readFile "input.txt"
+    let offsets = map (read::String -> Int) (lines input)
+    let offsetMap = fromList $ zip [0..(length offsets)] offsets
+    putStrLn . show $ stepThrough offsetMap 0
