43e2a7c0d419ff0834b88dcf89dba9564fc90d02
Joseph Roque <contact@josephroque.ca>
2018-12-03 23:50:13 -0500

    2018, day 3, python

---
+++ 2018/day_03/p1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Read the challenge input
+with open('input.txt', 'r') as input_file:
+  puzzle_input = input_file.readlines()
+
+claimed = {}
+overlapping_coords = 0
+
+for line in puzzle_input:
+    comp = line.split()
+
+    left = int(comp[2][:comp[2].index(',')])
+    top = int(comp[2][comp[2].index(',')+1:-1])
+    width = int(comp[3][:comp[3].index('x'):])
+    height = int(comp[3][comp[3].index('x')+1:])
+
+    for x in range(width):
+        for y in range(height):
+            spot = (left + x, top + y)
+            if spot in claimed and claimed[spot] == False:
+                overlapping_coords += 1
+                claimed[spot] = True
+            elif spot not in claimed:
+                claimed[spot] = False
+
+print('There are', overlapping_coords, 'points that have been claimed two or more times!')
---
+++ 2018/day_03/p2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# Read the challenge input
+with open('input.txt', 'r') as input_file:
+  puzzle_input = input_file.readlines()
+
+claimed = {}
+
+for line in puzzle_input:
+    comp = line.split()
+
+    left = int(comp[2][:comp[2].index(',')])
+    top = int(comp[2][comp[2].index(',')+1:-1])
+    width = int(comp[3][:comp[3].index('x'):])
+    height = int(comp[3][comp[3].index('x')+1:])
+
+    for x in range(width):
+        for y in range(height):
+            spot = (left + x, top + y)
+            if spot in claimed:
+                claimed[spot] += 1
+            else:
+                claimed[spot] = 1
+
+for line in puzzle_input:
+    comp = line.split()
+
+    claim_id = comp[0][1:]
+    left = int(comp[2][:comp[2].index(',')])
+    top = int(comp[2][comp[2].index(',')+1:-1])
+    width = int(comp[3][:comp[3].index('x'):])
+    height = int(comp[3][comp[3].index('x')+1:])
+
+    valid = True
+    for x in range(width):
+        for y in range(height):
+            spot = (left + x, top + y)
+            if claimed[spot] > 1:
+                valid = False
+    if valid:
+        print('The only claim that doesn\'t overlap with any others is:', claim_id)
+        break
