333ac7f42465d47d78050a08a6c4e4868e990b7a
Joseph Roque <contact@josephroque.ca>
2018-12-04 10:24:51 -0500

    Python utility functions

---
+++ util/util.py
@@ -0,0 +1,24 @@
+# Reading challenge input
+
+def get_test_lines():
+    return get_lines(name='../test.txt')
+
+def get_test_file():
+    return get_file(name='../test.txt')
+
+def get_file(name='../input.txt'):
+    with open(name) as input_file:
+        return input_file.read()
+
+def get_lines(name='../input.txt'):
+    with open(name) as input_file:
+        return input_file.readlines()
+
+def get_nums_by_line():
+    return [[int(match) for match in re.findall(r'-?\d+', line)] for line in get_lines()]
+
+def get_test_nums_by_line():
+    return [[int(match) for match in re.findall(r'-?\d+', line)] for line in get_test_lines()]
+
+def get_nums_from_line(line):
+    return [int(match) for match in re.findall(r'-?\d+', line)]
