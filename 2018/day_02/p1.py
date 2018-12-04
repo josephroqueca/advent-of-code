ca7e4589fd88fc1891605912e59498e45527194c
Joseph Roque <contact@josephroque.ca>
2018-12-04 01:05:01 -0500

    2018. day 2, python

---
+++ 2018/day_02/p1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Read the challenge input
+with open('input.txt', 'r') as input_file:
+  lines_input = input_file.readlines()
+
+ids_with_pairs =set()
+ids_with_triplets = set()
+
+for box_id in lines_input:
+    id_letters = {}
+    for letter in box_id:
+        if letter in id_letters:
+            id_letters[letter] += 1
+        else:
+            id_letters[letter] = 1
+
+    for letter in id_letters:
+        if id_letters[letter] == 2:
+            ids_with_pairs.add(box_id)
+        if id_letters[letter] == 3:
+            ids_with_triplets.add(box_id)
+
+print('The checksum is', len(ids_with_pairs) * len(ids_with_triplets))
---
+++ 2018/day_02/p2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Read the challenge input
+with open('input.txt', 'r') as input_file:
+  lines_input = input_file.readlines()
+
+ids_with_pairs =set()
+ids_with_triplets = set()
+ids = []
+
+def solve_p2():
+    for primary_box_id in lines_input:
+        for secondary_box_id in lines_input:
+            if primary_box_id == secondary_box_id: continue
+            for i in range(len(primary_box_id)):
+                adjusted_primary = primary_box_id[0:i] + primary_box_id[i+1:]
+                adjusted_secondary = secondary_box_id[0:i] + secondary_box_id[i+1:]
+                if adjusted_primary == adjusted_secondary:
+                    print('The common letters between your correct box ids are:', adjusted_primary)
+                    return
+
+solve_p2()
