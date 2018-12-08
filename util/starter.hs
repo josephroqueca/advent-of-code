ddea4494cadc33290d97248028f1a6bbb1fa6d43
Joseph Roque <contact@josephroque.ca>
2018-12-07 21:46:36 -0500

    Starter files

---
+++ util/starter.hs
@@ -0,0 +1,10 @@
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.List (sort)
+
+main :: IO()
+main = do
+    input <- readFile "../input.txt"
+    print input
--- util/starter.py
+++ util/starter.py
@@ -1,11 +1,18 @@
-# Reading challenge input
+#!/usr/bin/env python3
 
+import re
+import json
+import itertools
+import hashlib
+
+# Reading test input
 def get_test_lines():
     return get_lines(name='../test.txt')
 
 def get_test_file():
     return get_file(name='../test.txt')
 
+# Reading challenge input
 def get_file(name='../input.txt'):
     with open(name) as input_file:
         return input_file.read()
@@ -22,3 +29,4 @@ def get_test_nums_by_line():
 
 def get_nums_from_line(line):
     return [int(match) for match in re.findall(r'-?\d+', line)]
+
