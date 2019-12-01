8587d6f0b99b872c24bb3562ed768888b3b5fe07
Joseph Roque <contact@josephroque.ca>
2019-12-01 10:35:45 +0100

    Update Rust template

--- util/rust/supporting_files/src/main.rs
+++ util/rust/supporting_files/src/main.rs
@@ -1,20 +1,51 @@
 use std::env;
+use std::fs;
+use std::path::Path;
 
-fn part_one() {
-  println!("Hello, world! 1");
+/* Part 1
+================================================= */
+
+fn part_one(input: String) {
+    println!("{}", input);
 }
 
-fn part_two() {
-  println!("Hello, world! 2")
+/* Part 2
+================================================= */
+
+fn part_two(input: String) {
+  println!("{}", input);
 }
+
+/* Main + Input
+================================================= */
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
-  let part = &args[1];
+    let args: Vec<String> = env::args().collect();
+    let part = &args[1];
+    let input = get_input();
+    match part.as_str() {
+        "1" => part_one(input),
+        "2" => part_two(input),
+        _ => println!("Only parts 1 and 2 exist...")
+    }
+}
 
-  match part.as_str() {
-    "1" => part_one(),
-    "2" => part_two(),
-    _ => println!("Only parts 1 and 2 exist...")
-  }
+fn get_test_input() -> String {
+    let input_file = Path::new("../test.txt");
+
+    if input_file.exists() {
+        return fs::read_to_string(input_file).unwrap();
+    }
+
+    return String::new();
+}
+
+fn get_input() -> String {
+    let input_file = Path::new("../input.txt");
+
+    if input_file.exists() {
+        return fs::read_to_string(input_file).unwrap();
+    }
+
+    return String::new();
 }
