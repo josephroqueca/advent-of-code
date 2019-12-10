f6e1fb9ce62234ab0c913c0bb95cc152e0dca99a
Joseph Roque <contact@josephroque.ca>
2019-12-10 02:58:33 +0100

    Large number tests

--- util/rust/opcode_computer/src/lib.rs
+++ util/rust/opcode_computer/src/lib.rs
@@ -485,4 +485,16 @@ mod tests {
             Program::from_str("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99");
         assert_eq!(quine.run().output(), quine.state.original_memory);
     }
+
+    #[test]
+    fn test_sixteen_digits() {
+        let mut sixteen = Program::from_str("1102,34915192,34915192,7,4,7,99,0");
+        assert_eq!(sixteen.run().output().pop().unwrap().to_string().len(), 16);
+    }
+
+    #[test]
+    fn test_large_value() {
+        let mut large = Program::from_str("104,1125899906842624,99");
+        assert_eq!(large.run().output().pop().unwrap(), 1125899906842624);
+    }
 }
