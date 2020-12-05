5f89d20c963fa3786f05d13f0c07707bf0da4863
Joseph Roque <github@josephroque.ca>
2020-12-05 00:36:29 -0500

    2020, day 5, python

---
+++ 2020/day_05/python/p1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import os
+import re
+
+test_input = False
+
+SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
+FILENAME = '{}/../{}.txt'.format(SCRIPT_PATH, 'test' if test_input else 'input')
+
+
+def get_lines():
+    with open(FILENAME) as f:
+        return [line.strip() for line in f.readlines()]
+
+
+# Solution
+
+
+def get_seat_id(boarding_pass):
+    seat_row, seat_col = None, None
+    row_lower, row_upper = 0, 127
+    col_lower, col_upper = 0, 7
+
+    for index, direction in enumerate(boarding_pass):
+        row_mid = (row_lower + row_upper) // 2
+        col_mid = (col_lower + col_upper) // 2
+
+        if direction == 'F':
+            row_upper = row_mid
+            if index == 6:
+                seat_row = row_upper
+        elif direction == 'B':
+            row_lower = row_mid + 1
+            if index == 6:
+                seat_row = row_lower
+        elif direction == 'L':
+            col_upper = col_mid
+            if index == 9:
+                seat_col = col_upper
+        elif direction == 'R':
+            col_lower = col_mid + 1
+            if index == 9:
+                seat_col = col_lower
+
+    return seat_row * 8 + seat_col
+
+
+highest_seat_id = max([get_seat_id(boarding_pass) for boarding_pass in get_lines()])
+print('The highest seat id is {}'.format(highest_seat_id))
