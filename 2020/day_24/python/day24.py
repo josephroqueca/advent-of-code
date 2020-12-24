feb83348aeb65efc842d1ac55916421d4ef9bfad
Joseph Roque <joseph.roque@shopify.com>
2020-12-24 01:59:28 -0500

    Support hexagonal positions

--- 2020/day_24/python/day24.py
+++ 2020/day_24/python/day24.py
@@ -3,31 +3,9 @@ import re
 from aoc import Position, flatten
 from copy import copy
 
+Position.hexagonal = True
+
 data = aoc.load(year=2020, day=24)
-
-def east(pos):
-    pos.x, pos.y = pos.x - 1, pos.y + 1
-    return pos
-
-def northwest(pos):
-    pos.x, pos.z = pos.x + 1, pos.z - 1
-    return pos
-
-def northeast(pos):
-    pos.y, pos.z = pos.y + 1, pos.z - 1
-    return pos
-
-def southwest(pos):
-    pos.y, pos.z = pos.y - 1, pos.z + 1
-    return pos
-
-def southeast(pos):
-    pos.x, pos.z = pos.x - 1, pos.z + 1
-    return pos
-
-def west(pos):
-    pos.x, pos.y = pos.x + 1, pos.y - 1
-    return pos
 
 # Part 1
 
@@ -36,17 +14,17 @@ for tile in data.lines():
     to_flip = Position(0, 0, 0)
     for c in re.findall(r'e|w|se|sw|ne|nw', tile):
         if c == 'e':
-            east(to_flip)
+            to_flip.move_east()
         elif c == 'w':
-            west(to_flip)
+            to_flip.move_west()
         elif c == 'ne':
-            northeast(to_flip)
+            to_flip.move_northeast()
         elif c == 'nw':
-            northwest(to_flip)
+            to_flip.move_northwest()
         elif c == 'se':
-            southeast(to_flip)
+            to_flip.move_southeast()
         elif c == 'sw':
-            southwest(to_flip)
+            to_flip.move_southwest()
 
     black.add(to_flip) if to_flip not in black else black.remove(to_flip)
 
@@ -55,29 +33,19 @@ print(p1_solution)
 
 # Part 2
 
-def adjacent(pos):
-    return (
-        east(copy(pos)),
-        northwest(copy(pos)),
-        northeast(copy(pos)),
-        southwest(copy(pos)),
-        southeast(copy(pos)),
-        west(copy(pos)),
-    )
-
 def cycle(black, seen):
     next_black = set()
     for tile in seen:
-        adj = adjacent(tile)
+        adj = tile.adjacent()
 
         adjacent_black = len([1 for x in adj if x in black])
         if tile in black and not (adjacent_black == 0 or adjacent_black > 2):
             next_black.add(tile)
         if tile not in black and adjacent_black == 2:
             next_black.add(tile)
-    return (next_black, set(flatten([adjacent(c) for c in next_black])))
+    return (next_black, set(flatten([c.adjacent() for c in next_black])))
 
-seen = black | set(flatten([adjacent(c) for c in black]))
+seen = black | set(flatten([c.adjacent() for c in black]))
 for _ in range(100):
     black, seen = cycle(black, seen)
 
--- util/python/util/position.py
+++ util/python/util/position.py
@@ -1,14 +1,20 @@
+from copy import copy
+
 class Position:
     _xlim=None
     _ylim=None
     _zlim=None
     _wlim=None
+    hexagonal=False
 
     def __init__(self, x, y, z=None, w=None):
         self.x = x
         self.y = y
         self.z = z
         self.w = w
+
+    def __copy__(self):
+        return type(self)(self.x, self.y, self.z, self.w)
 
     def __attrs(self):
         if self.z is None:
@@ -47,6 +53,8 @@ class Position:
             (Position._wlim is None or pos.w in Position._wlim)
 
     def adjacent(self):
+        if Position.hexagonal:
+            return self.hexagonal_adjacent()
         adj = []
         for xd in range(-1, 2):
             for yd in range(-1, 2):
@@ -63,3 +71,89 @@ class Position:
                         if xd == yd == zd == wd == 0: continue
                         adj.append(Position(self.x + xd, self.y + yd, self.z + zd, self.w + wd))
         return [a for a in adj if Position.is_within_limits(adj)]
+
+    def hexagonal_adjacent(self):
+        return [
+            self.east(),
+            self.west(),
+            self.northeast(),
+            self.northwest(),
+            self.southeast(),
+            self.southwest(),
+        ]
+
+    def east(self):
+        east = copy(self)
+        east.move_east()
+        return east
+
+    def move_east(self):
+        if Position.hexagonal:
+            self.x, self.y = self.x - 1, self.y + 1
+        else:
+            self.x -= 1
+
+    def west(self):
+        west = copy(self)
+        west.move_west()
+        return west
+
+    def move_west(self):
+        if Position.hexagonal:
+            self.x, self.y = self.x + 1, self.y - 1
+        else:
+            self.x += 1
+
+    def north(self):
+        north = copy(self)
+        north.move_north()
+        return north
+
+    def move_north(self):
+        if Position.hexagonal: return
+        self.y -= 1
+
+    def south(self):
+        south = copy(self)
+        south.move_south()
+        return south
+
+    def move_south(self):
+        if Position.hexagonal: return
+        self.y += 1
+
+    def northwest(self):
+        northwest = copy(self)
+        northwest.move_northwest()
+        return northwest
+
+    def move_northwest(self):
+        if not Position.hexagonal: return
+        self.x, self.z = self.x + 1, self.z - 1
+
+    def northeast(self):
+        northeast = copy(self)
+        northeast.move_northeast()
+        return northeast
+
+    def move_northeast(self):
+        if not Position.hexagonal: return
+        self.y, self.z = self.y + 1, self.z - 1
+
+    def southwest(self):
+        southwest = copy(self)
+        southwest.move_southwest()
+        return southwest
+
+    def move_southwest(self):
+        if not Position.hexagonal: return
+        self.y, self.z = self.y - 1, self.z + 1
+
+    def southeast(self):
+        southeast = copy(self)
+        southeast.move_southeast()
+        return southeast
+
+    def move_southeast(self):
+        if not Position.hexagonal: return
+        self.x, self.z = self.x - 1, self.z + 1
